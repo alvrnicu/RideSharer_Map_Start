44f1dce4c127f2cbeaf8604544239c800e2e8f81
Alvin Cheon <108594108+alvrnicu@users.noreply.github.com>
2023-09-30 01:35:19 -0400

    Initial commit

---
+++ RideSharer/MapViewController.swift
@@ -0,0 +1,78 @@
+//
+//  MapViewController.swift
+//  RideSharer
+//
+//
+import MapKit
+import UIKit
+
+class MapViewController: UIViewController {
+    //Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //Variables
+    let locationManager = CLLocationManager()
+    
+    //Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkLocationServces()
+    }
+    
+    func checkLocationServces() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManagerDidChangeAuthorization()
+        } else {
+            //Do something to let users know why they need to turn it on
+            alertLocationPermNeeded()
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization() {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse, .authorizedAlways:
+            mapView.showsUserLocation = true
+        case .denied, .restricted:
+            alertLocationPermNeeded()
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            mapView.showsUserLocation = true
+        @unknown default:
+            break
+        }
+    }
+    
+    //Alert functionality
+    func alertLocationPermNeeded() {
+        let alert = UIAlertController(
+            title: "Need Location Access Permissions",
+            message: "GPS access is restricted. Go to Location Services under Privacy to enable GPS to have access to tracking.",
+            preferredStyle: UIAlertController.Style.alert
+        )
+        
+        alert.addAction(UIAlertAction(title: "Cancel",
+                                      style: .default,
+                                      handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "Allow Location Access",
+                                      style: .cancel,
+                                      handler: { (alert) -> Void in
+            UIApplication.shared.open(URL(string: "app-settings:root=LOCATION_SERVICES")!,
+                                                                options: [:],
+                                                                completionHandler: nil)
+        }))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func alertDeniedLocation() {
+        let alert = UIAlertController(
+            title: "Denied",
+            message: "You have denied Location Access. Tracking unavailable.",
+            preferredStyle: UIAlertController.Style.alert
+            )
+        alert.addAction(UIAlertAction(title: "OK", 
+                                      style: UIAlertAction.Style.default,
+                                      handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+}
